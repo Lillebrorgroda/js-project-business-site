472d8ca98280fea0bbdfde0886325bfe5743127d
Lillebrorgroda <therese.modig@hotmail.com>
2025-02-19 14:52:53 +0100

    Fixed clean code in js file

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-const menuIcon = document.getElementById("menu-icon");
-const navLinks = document.getElementById("nav-links");
-const header = document.querySelector("header");
-const originalParent = navLinks.parentNode; // Spara ursprungliga placeringen
+const menuIcon = document.getElementById("menu-icon")
+const navLinks = document.getElementById("nav-links")
+const header = document.querySelector("header")
+const originalParent = navLinks.parentNode // Spara ursprungliga placeringen
 
 menuIcon.addEventListener("click", () => {
   if (!navLinks.classList.contains("active")) {
-    document.body.insertBefore(navLinks, header); // Flytta ovanför header
+    document.body.insertBefore(navLinks, header) // Flytta ovanför header
   } else {
-    originalParent.insertBefore(navLinks, header.nextSibling); // Flytta tillbaka under navbar
+    originalParent.insertBefore(navLinks, header.nextSibling) // Flytta tillbaka under navbar
   }
 
-  navLinks.classList.toggle("active");
-});
+  navLinks.classList.toggle("active")
+})
 
