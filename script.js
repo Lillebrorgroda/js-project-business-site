ddcce37f790575d307c94c3a37c3618a6ed4de98
Lillebrorgroda <therese.modig@hotmail.com>
2025-02-19 14:59:29 +0100

    Removed one article and fixed comments

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 const menuIcon = document.getElementById("menu-icon")
 const navLinks = document.getElementById("nav-links")
 const header = document.querySelector("header")
-const originalParent = navLinks.parentNode // Spara ursprungliga placeringen
+const originalParent = navLinks.parentNode // Save the original placing of the navlinks
 
 menuIcon.addEventListener("click", () => {
   if (!navLinks.classList.contains("active")) {
-    document.body.insertBefore(navLinks, header) // Flytta ovanför header
+    document.body.insertBefore(navLinks, header) // Move the navlinks above header
   } else {
-    originalParent.insertBefore(navLinks, header.nextSibling) // Flytta tillbaka under navbar
+    originalParent.insertBefore(navLinks, header.nextSibling) // Move the navlinks back to the original placing
   }
 
   navLinks.classList.toggle("active")
