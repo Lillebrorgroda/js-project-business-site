b4d7180e97169367756ffc602a0b1fde496856cb
Lillebrorgroda <therese.modig@hotmail.com>
2025-02-19 14:47:28 +0100

    Fixed hamburgermenu

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 const menuIcon = document.getElementById("menu-icon");
 const navLinks = document.getElementById("nav-links");
+const header = document.querySelector("header");
+const originalParent = navLinks.parentNode; // Spara ursprungliga placeringen
 
 menuIcon.addEventListener("click", () => {
+  if (!navLinks.classList.contains("active")) {
+    document.body.insertBefore(navLinks, header); // Flytta ovanför header
+  } else {
+    originalParent.insertBefore(navLinks, header.nextSibling); // Flytta tillbaka under navbar
+  }
+
   navLinks.classList.toggle("active");
 });
-
-
 
