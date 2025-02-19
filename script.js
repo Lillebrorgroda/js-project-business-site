29a7d70612a19a5fa4b51534caea8d064b2705dd
Lillebrorgroda <therese.modig@hotmail.com>
2025-02-19 14:06:46 +0100

    Uppdated hamburgermenu

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuIcon = document.getElementById("menu-icon");
-  const navLinks = document.getElementById("nav-links");
 
-  menuIcon.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
-  });
+const menuIcon = document.getElementById("menu-icon");
+const navLinks = document.getElementById("nav-links");
+
+menuIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
+
+
+
